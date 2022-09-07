f17e99e4330ff45046ecc2fdec5d2468a176e17f
djyaron <yaron@cmu.edu>
2022-09-07 16:06:23 -0400

    added a few things to stjames1.py

--- stjames1.py
+++ stjames1.py
@@ -82,8 +82,38 @@ mols = load_ani1_data()
 #%%
 test1 = mols[10000]
 
-Z = test1['atomic_numbers']
-R = test1['coordinates'].T
+Z1 = test1['atomic_numbers']
+R1 = test1['coordinates'].T
+
+
+f1 = ani1_feature(Z1, R1)
+
+test2 = mols[10001]
+
+Z2 = test2['atomic_numbers']
+R2 = test2['coordinates'].T
+
+
+f2 = ani1_feature(Z2, R2)
+
+C1 = f1[0,:]
+C2 = f2[0,:]
+
+plt.figure(1)
+plt.plot(C1,'r.')
+plt.figure(2)
+plt.plot(C2,'b.')
+plt.figure(3)
+plt.plot(C1-C2,'g.')
+
+C1n = C1/np.linalg.norm(C1)
+C2n = C2/np.linalg.norm(C2)
+diag1 = np.dot(C1n.T, C1n)
+diag2 = np.dot(C2n.T, C2n)
+diff = np.dot(C1n.T, C2n)
 
 #%%
-features = ani1_feature(Z, R)
+dft = np.array([x['targets']['hd'] for x in mols]) * 627.0 # hartree to kcal/mol
+cc = np.array([x['targets']['cc'] for x in mols]) * 627.0
+plt.figure(10)
+plt.plot(dft-cc, 'r.')
